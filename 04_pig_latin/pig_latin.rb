885412905e0b3c86db1c8dfc2e068e8ad6258d5e
Sai <niassain@gmail.com>
2017-01-08 21:18:41 -0500

    completed questions

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
 #write your code here
+
+def translate(english)
+	english = english.split
+	vowel = ['a', 'e', 'i', 'o', 'u']
+	latin = Array.new
+	to_latin = Proc.new do |word|
+		if vowel.include? word[0] 
+			word += 'ay'
+		elsif 'sch'.include? word[0..2]
+			word = to_latin.call(word[3..-1]+word[0..2])
+		elsif 'qu'.include? word[0..1]
+			word = to_latin.call(word[2..-1]+word[0..1])
+		else
+			word = to_latin.call(word[1..-1]+word[0])
+		end
+		word
+	end
+
+	english.each do |i|
+		latin.push(to_latin.call(i))
+	end
+	latin.join(' ')
+
+end
+
+translate "eat pie"
